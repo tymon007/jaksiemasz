9e2c0d16d7bca3eb48fa6e18b5fd741fab7ac6dc
Albdarion <ukiteroamano968@gmail.com>
2020-10-20 11:09:15 +0200

    Charts were added

--- main_page/urls.py
+++ main_page/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     re_path(r'^me/$',                 views.me,                 name="me"),
     re_path(r'^credits/$',            views.credits,            name="credits"),
     re_path(r'^lekarze/$',            views.lekarze,            name="lekarze"),
+    re_path(r'^wykresy/$',            views.wykresy,            name="wykresy"),
     re_path(r'^.*$',                  views.http_not_found,     name="not_found")
 ]
